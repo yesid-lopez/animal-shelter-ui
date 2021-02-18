16f34fef3b13d3cedf30319b46632074e278fac5
Yesid Leonardo López Sierra <yesid.lopez@correo.icesi.edu.co>
2021-02-18 14:46:16 -0500

    Feature/heroku setup (#3)
    * add list
    
    * update list
    
    * fix key index for list
    
    * add animal controller
    
    * feaeture: heroku integration
    
    * add EOF

--- controllers/AnimalsController.js
+++ controllers/AnimalsController.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 export const AnimalController = {
     async list() {
-        return await axios.get('http://localhost:8080/animals');
+        return await axios({
+            method: 'GET',
+            baseURL: process.env.API_URL,
+            url: 'animals'
+        });
     }
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+import dotenv from 'dotenv'
 import '../styles/globals.css'
+dotenv.config()
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
