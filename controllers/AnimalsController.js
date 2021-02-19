e80a4d6af385086637fbcc8e3a6eeeb1733cf902
Yesid Leonardo López Sierra <yesid.lopez@correo.icesi.edu.co>
2021-02-18 22:19:58 -0500

    add register controller (#6)
    * add register controller
    
    * add EOF

--- controllers/AnimalsController.js
+++ controllers/AnimalsController.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
 export const AnimalController = {
-    async list() {
-        return await axios({
+    register(animal) {
+        return axios({
+            url: `animals`,
+            baseURL: process.env.API,
+            data: animal,
+            method: 'POST'
+        })
+    },
+    list() {
+        return axios({
             method: 'GET',
-            baseURL: process.env.API_URL,
+            baseURL: process.env.API,
             url: 'animals'
         });
     }
