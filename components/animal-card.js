a95c60676ddc432fbec229af14adfc8024e61856
saradrada <sara.ortiz@correo.icesi.edu.co>
2021-03-02 23:55:41 -0500

    add attributes to elements

--- components/animal-card.js
+++ components/animal-card.js
@@ -1,51 +1,73 @@
-import { faHeart, faMars, faVenus, faVirus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import { VaccineList } from './vaccines-list';
+import {
+  faHeart,
+  faMars,
+  faVenus,
+  faVirus,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { VaccineList } from "./vaccines-list";
 
 const useStyles = makeStyles({
-    root: {
-    },
+  root: {},
 });
 
 export const AnimalCard = ({ name, breed, gender, isVaccinated, vaccines }) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Card className={classes.root} data-testid={`${name}-container`}>
-            <CardContent>
-                <CardMedia
-                    component="img"
-                    alt="Contemplative Reptile"
-                    height="140"
-                    image="/img/cat.png"
-                    title="Contemplative Reptile"
-                />
-                <CardContent>
-                    <Grid container spacing={1}>
-                        <Grid item xs={8}>
-                            <Typography gutterBottom variant="h4" component="h2">
-                                {name}
-                            </Typography>
-                            <Typography gutterBottom variant="h6" component="h2">
-                                {breed}
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={2}>
-                            {gender == "Male" ? <FontAwesomeIcon icon={faMars} /> : <FontAwesomeIcon icon={faVenus} />}
-                        </Grid>
-                        <Grid item xs={2}>
-                            {isVaccinated ? <FontAwesomeIcon icon={faHeart} /> : <FontAwesomeIcon icon={faVirus} />}
-                        </Grid>
-                    </Grid>
-                    <VaccineList vaccines={vaccines} />
-                </CardContent>
-            </CardContent>
-        </Card >
-    );
-}
+  return (
+    <Card className={classes.root} data-testid={`${name}-container`}>
+      <CardContent>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="/img/cat.png"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid item xs={8}>
+              <Typography
+                gutterBottom
+                variant="h4"
+                component="h2"
+                name="name-cat"
+              >
+                {name}
+              </Typography>
+              <Typography
+                gutterBottom
+                variant="h6"
+                component="h2"
+                name="breed-cat"
+              >
+                {breed}
+              </Typography>
+            </Grid>
+            <Grid item xs={2} name="gender-cat">
+              {gender == "Male" ? (
+                <FontAwesomeIcon icon={faMars} />
+              ) : (
+                  <FontAwesomeIcon icon={faVenus} />
+                )}
+            </Grid>
+            <Grid item xs={2} name="is-vaccined-cat">
+              {isVaccinated ? (
+                <FontAwesomeIcon icon={faHeart} />
+              ) : (
+                  <FontAwesomeIcon icon={faVirus} />
+                )}
+            </Grid>
+          </Grid>
+          <VaccineList vaccines={vaccines} />
+        </CardContent>
+      </CardContent>
+    </Card>
+  );
+};
--- components/vaccines-list.js
+++ components/vaccines-list.js
@@ -1,35 +1,36 @@
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Avatar from '@material-ui/core/Avatar';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
-    },
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
 }));
 
 export const VaccineList = ({ vaccines }) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (<List className={classes.root}>
-
-        {vaccines.map(vaccine =>
-            <ListItem key={vaccine}>
-                <ListItemAvatar>
-                    <Avatar>
-                        <FontAwesomeIcon icon={faCheckCircle} />
-                    </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={vaccine} />
-            </ListItem>
-        )}
-    </List>)
-}
+  return (
+    <List className={classes.root} name="vaccines-list-cat">
+      {vaccines.map((vaccine) => (
+        <ListItem key={vaccine}>
+          <ListItemAvatar>
+            <Avatar>
+              <FontAwesomeIcon icon={faCheckCircle} />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={vaccine} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
