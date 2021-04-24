782ff3146ee70f65c886ccab56074c0fb1fdd269
Laura Hincapie <laura@pulpowms.com>
2021-04-23 20:40:38 -0500

    Contract test and publish contract

--- test/contract/specs/init-pact.js
+++ test/contract/specs/init-pact.js
@@ -1,14 +1,16 @@
-import { Pact } from '@pact-foundation/pact';
-import path from 'path';
+import { Pact } from "@pact-foundation/pact";
+import path from "path";
 
-const consumerName ='AnimalShelterFront';
+const consumerName = 'AnimalShelterFront';
 const providerName = 'AnimalShelterBack';
 
-export const provider = new Pact({
-    consumer: consumerName,
-    provider: providerName,
-    port: 8080,
-    cors: true,
-    log: path.resolve(process.cwd(), './test/contract/logs', `${consumerName}-${providerName}.log`),
-    dir: path.resolve(process.cwd(), './test/contract/pacts'),
-})
+export function getProvider() {
+    return new Pact({
+        consumer: consumerName,
+        provider: providerName,
+        port: 8090,
+        cors: true,
+        log: path.resolve(process.cwd(),'./test/contract/logs', `${consumerName}-${providerName}.log`),
+        dir: path.resolve(process.cwd(), './test/contract/pacts')
+    })
+}
---
+++ test/contract/specs/list-animal.contract.test.js
@@ -0,0 +1,46 @@
+import { getProvider } from "./init-pact";
+import { AnimalController } from "../../../controllers";
+import { Matchers } from "@pact-foundation/pact";
+
+const provider = getProvider();
+describe('Given an Animal Service', () => {
+    describe('When a request to list all the animals is made', () => {
+        beforeAll(async () => {
+            await provider.setup();
+            await provider.addInteraction({
+                state: 'there are animals',
+                uponReceiving: 'a request to get all animals',
+                withRequest: {
+                    method: 'GET',
+                    path: '/animals'
+                },
+                willRespondWith: {
+                    status: 200,
+                    body: Matchers.eachLike(
+                        {
+                            breed: Matchers.like("Bengali"),
+                            gender: Matchers.term(
+                                {generate:"Female", matcher:"Female|Male"}
+                            ),
+                            isVaccinated: Matchers.boolean(true),
+                            name: Matchers.string("Manchas"),
+                            vaccines: Matchers.eachLike(["rabia"], {min: 1})
+                        },
+                        {min: 1}
+                    )
+                }
+            });
+        });
+
+        it("Then it should return the right data", async() => {
+            const response = await AnimalController.list();
+            expect(response.data).toMatchSnapshot();
+
+            await provider.verify();
+        });
+
+        afterAll(async () => {
+            await provider.finalize();
+        });
+    });
+});
